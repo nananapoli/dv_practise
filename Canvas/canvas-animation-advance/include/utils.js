1781e3798445667babbaa311dff7e4c049ed6eca
napoli <1409260585@qq.com>
2019-11-07 09:05:07 +0800

    feat: add touch

--- Canvas/canvas-animation-advance/include/utils.js
+++ Canvas/canvas-animation-advance/include/utils.js
@@ -67,6 +67,53 @@ window.utils.captureMouse = function (element) {
   return mouse;
 }
 
+window.utils.captureTouch = function (element) {
+  var touch = {x: null, y: null, isPressed: false, event: null},
+      body_scrollLeft = document.body.scrollLeft,
+      element_scrollLeft = document.documentElement.scrollLeft,
+      body_scrollTop = document.body.scrollTop,
+      element_scrollTop = document.documentElement.scrollTop,
+      offsetLeft = element.offsetLeft,
+      offsetTop = element.offsetTop;
+
+  element.addEventListener('touchstart', function (event) {
+    touch.isPressed = true;
+    setPosition(event)
+  }, false);
+
+  element.addEventListener('touchend', function (event) {
+    touch.isPressed = false;
+    touch.x = null;
+    touch.y = null;
+    touch.event = event;
+  }, false);
+  
+  element.addEventListener('touchmove', function (event) {
+    setPosition(event)
+  }, false);
+
+  function setPosition (event) {
+    var x, y,
+        touch_event = event.touches[0]; //first touch
+    
+    if (touch_event.pageX || touch_event.pageY) {
+      x = touch_event.pageX;
+      y = touch_event.pageY;
+    } else {
+      x = touch_event.clientX + body_scrollLeft + element_scrollLeft;
+      y = touch_event.clientY + body_scrollTop + element_scrollTop;
+    }
+    x -= offsetLeft;
+    y -= offsetTop;
+    
+    touch.x = x;
+    touch.y = y;
+    touch.event = event;
+  }
+  
+  return touch;
+};
+
 window.utils.range = function (min, max) {
   return Math.floor((max - min) * Math.random() + min);
 }
