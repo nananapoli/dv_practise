846c6efa8aa22fab8659120c56b0a09655f07635
napoli <1409260585@qq.com>
2019-11-29 13:51:55 +0800

    feat: kickoff

--- Canvas/chilly-snow/src/index.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
